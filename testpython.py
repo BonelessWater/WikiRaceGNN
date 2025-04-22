f474935fcc06f7359886090a0cacd2565c372a53
BonelessWater <domdd305@gmail.com>
2025-04-22 14:42:11 -0400

    cleaning repository

--- testpython.py
+++
@@ -1,9 +0,0 @@
-# test_imports.py
-try:
-    import gensim
-    print(f"Gensim found! Version: {gensim.__version__}")
-except ImportError as e:
-    print(f"Error importing gensim: {e}")
-
-import sys
-print(f"Python path: {sys.path}")
